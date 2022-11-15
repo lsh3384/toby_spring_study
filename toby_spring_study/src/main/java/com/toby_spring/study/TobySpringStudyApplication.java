d7c75fefeee58b41d4f092b815ba5264c1195347
lee <lsh3384@gmail.com>
2022-11-15 23:36:52 +0900

    added User Class

---
+++ toby_spring_study/src/main/java/com/toby_spring/study/TobySpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.toby_spring.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobySpringStudyApplication.class, args);
+	}
+
+}
---
+++ toby_spring_study/src/test/java/com/toby_spring/study/TobySpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby_spring.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
